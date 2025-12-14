5b5b5bb71973cadaed74d8665fe654da3d6ac84a
Faisal-Khan-dev <faisalkhan24687531@gmail.com>
2025-12-14 13:22:43 +0500

    bcdsvhf

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,3 @@
+
 // export const base_url = "http://localhost:5000/api" // dev mode
-export const base_url = "https://quotation-management-system-backend.vercel.app/api"  // pro modelog
+export const base_url = `${import.meta.env.VITE_API_URL}/api`  // pro modelog
